7acf1a8529db20362e0f60c2f3fa740164e0d6f4
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-02-10 19:11:52 -0800

    Fully-finished C & Python implementations

--- projects/project1/chatClient.cpp
+++ projects/project1/chatClient.cpp
@@ -7,9 +7,117 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 
+// connects to server at port and returns the socket
+int init(int port){
+	struct sockaddr_in address;
+	int sock = 0;
+	struct sockaddr_in serv_addr;
+
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+	  printf("\n ERROR: Could not create socket");
+	  return -1;
+	}
+
+	memset(&serv_addr, '0', sizeof(serv_addr));
+
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(port);
+
+	// convert ip to binary
+	if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0){
+	  printf("\nInvalid address\n");
+	  return -1;
+	}
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0){
+	  printf("\nConnection Failed \n");
+	  return -1;
+	}
+
+	return sock;
+}
+
+// sends and receives messages to/from the server specified at socket sock
+int chat(int sock){
+  //char message[] = "Ello m8";
+  char buffer[500] = {0};
+  char message[510] = {0};
+  int reply;
+  
+  // get user handle
+  char handle[11];
+  printf("handle: ");
+  fgets(handle, 10, stdin);
+  printf("Hello, %s.", handle);
+  // get rid of newline
+  handle[strlen(handle)-1] = 0;
+
+  while (true){
+    // add handle to message
+    strcpy(message, handle);
+    strcat(message, "> ");
+
+    // get user message
+    printf("message: ");
+    fgets(buffer, 500, stdin);
+    
+    // get rid of newline
+    buffer[strlen(buffer)-1] = 0;
+
+    // check for quit command
+    if (strcmp(buffer, "\\quit") == 0){
+      return 1;
+    }
+
+    // add user input to message
+    strcat(message, buffer);
+
+    // send message
+    send(sock, message, strlen(message), 0);
+
+    // clear buffer
+    memset(buffer, 0, sizeof(buffer));
+    memset(message, 0, sizeof(message));
+
+    // read reply
+    reply = read(sock, buffer, 512);
+    printf("%s\n", buffer);
+  }
+  return 0;
+}
+
+// NOT USED: Reads port from file
+int getPort(){
+  FILE *fp;
+  static char buffer[10];
+  int i = 0;
+  char c;
+
+  fp = fopen("port.txt", "r");
+  if (fp){
+    while ((c = getc(fp)) != EOF){
+      buffer[i] = c;
+      i++;
+    }
+    fclose(fp);
+  }
+  return atoi(buffer);
+}
+
+// driver function; initializes connection and starts chat
 int main(int argc, char const *argv[])
 {
-	/* code */
-	return 0;
+  int port = atoi(argv[1]);
+  printf("%d\n",port);
+  int sock = init(port);
+  if (sock > 0)
+    chat(sock);
+  return 0;
 }
+
+/*
+ * Special thanks to https://www.geeksforgeeks.org/socket-programming-cc/ for the boilerplate code.
+ */
--- projects/project1/server2.py
+++ projects/project1/server2.py
@@ -1,49 +1,55 @@
 # server2.py
 # another more simple implementation
 
-import readPort
 import socket
+import sys
 
+# driver function; initializes and starts chat
+# increments port after client terminates
 def main():
+	port = int(sys.argv[1])
 	while(True):
-		conn = setup()
-		print conn
+		conn = setup(port)
+		#print conn
 		chat(conn)
+		port = port + 1;
 
+# sends and receives data on a socket connection conn
 def chat(conn):
 	while 1:
 		
 		data = conn.recv(512)
 		
-		
 		# decode data
 		rcv = repr(data)
+		#rcv = rcv.strip("'");
 
 		# check for terminated connection
 		if (len(rcv) == 2): # empty string
 			return
 
 		# print message from client
-		print rcv
+		print rcv.strip("\'");
 		message = raw_input("enter a message: ")
 		
 		# handle server self-quit
 		if (message == "\\quit"):
 			conn.sendall("Connection terminated")
-			conn.shutdown(socket.SHUT_RDWR)
 			quit()
 
 		# send server's message
 		conn.sendall("server> " + message)
+	
+	conn.shutdown(socket.SHUT_RDWR)
 	conn.close()
 
-def setup():
+# establishes a new TCP socket connection and returns the connection object conn
+def setup(port):
 	host = 'localhost'
-	port = readPort.getPort(True)
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port))
 	s.listen(1)
-	print "Waiting for connection..."
+	print "Waiting for connection on port %d..." % port;
 	conn, addr = s.accept()
 	print 'connection from ', addr
 	return conn
