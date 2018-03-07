efe40743810070d564beb95090ee1260cb4e33a1
Brock Smedley <2791467+BrockSmedley@users.noreply.github.com>
2018-03-06 23:34:51 -0800

    bugfix: client argument error handling

--- projects/project2/client.py
+++ projects/project2/client.py
@@ -15,30 +15,39 @@ def main():
   #print args
 
   argnames = ["server_host", "server_port", "command", "filename", "data_port"]
-
+  filename = ""
   if (args < 5):
     print "invalid arguments:\nhave %s\nmissing %s" % (argnames[:args-1], argnames[(args-1):])
+    exit(420)
+  elif (args < 6 and args >= 5):
+    if sys.argv[3] == "-l":
+      filename  = "0"
+      data_port = sys.argv[4]
+    else:
+      print "Invalid argumentsss"
+      exit(420)
+  elif sys.argv[3] == "-g":
+    filename	= sys.argv[4]
+    data_port	= sys.argv[5]
+  else:
+    print "Invalid arguments"
     exit(420)
 
   server_host 	= sys.argv[1]
   server_port 	= sys.argv[2]
   command	= sys.argv[3]
-  if command == "-l":
-    filename  = "00"
-    data_port = sys.argv[4]
-  else:
-    filename	= sys.argv[4]
-    data_port	= sys.argv[5]
-    if (os.path.exists(filename)):
-      print "File with that name already exists in this directory..."
-      exit(69)
+ 
+    
+  if (filename != "" and filename != "0" and os.path.exists(filename)):
+    print "File with that name already exists in this directory..."
+    exit(69)
       
 
   #host = socket.gethostname()
   print "opening control connection on port %s" % (server_port)
 
   csock = socket.socket()
-  csock.connect(('', int(server_port)))
+  csock.connect((server_host, int(server_port)))
   payload = [command, filename, data_port]
 
   for i in payload: 
@@ -53,16 +62,18 @@ def main():
   
   # open new connection to download data
   dsock = socket.socket()
-  dsock.connect(('', int(data_port)))
+  dsock.connect((server_host, int(data_port)))
   # no payload this time
 
   while (1):
     msg = dsock.recv(1024)
     if (msg):
       if (msg == "0xERR"):
-	print("ERROR: File does not exist on server.\n")
-      elif (not os.path.exists(filename)):
+	print "ERROR: File does not exist on server.\n"
+      elif (command == "-g" and not os.path.exists(filename)):
 	writefile(filename, msg)
+      else:
+	print msg
     else:
       break
 
